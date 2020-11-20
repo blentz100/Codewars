6ccc93313f6dcbdb227cbd7812b40185eb34cccb
Brendan Lentz <Brendan.Lentz@gmail.com>
2020-11-19 19:29:37 -0700

    added switch statement

--- parseInt.js
+++ parseInt.js
@@ -20,11 +20,72 @@ function parseInt(incomingString){
 
 	for (x = 0; x < modifiedString.length; x++){
 		console.log(modifiedString[x]);
-		
+		switch (modifiedString[x]){
+			case 'one':
+				answer += '1';
+				break;
+			case 'two':
+				answer += '2';
+				break;
+			case 'three':
+				answer += '3';
+				break;
+			case 'four':
+				answer += '4';
+				break;
+			case 'five':
+				answer += '5';
+				break;
+			case 'six':
+				answer += '6';
+				break;
+			case 'seven':
+				answer += '7';
+				break;
+			case 'eight':
+				answer += '8';
+				break;
+			case 'nine':
+				answer += '9';
+				break;
+			case 'ten':
+				answer += '10';
+				break;
+			case 'eleven':
+				answer += '11';
+				break;
+			case 'twelve':
+				answer += '12';
+				break;
+			case 'thirteen':
+				answer += '13';
+				break;
+			case 'fourteen':
+				answer += '14';
+				break;
+			case 'fifteen':
+				answer += '15';
+				break;
+			case 'sixteen':
+				answer += '16';
+				break;
+			case 'seventeen':
+				answer += '17';
+				break;
+			case 'eighteen':
+				answer += '18';
+				break;
+			case 'nineteen':
+				answer += '19';
+				break;
+			case 'twenty':
+				answer += '20';
+				break;
+		}
 
 
 	}
-
+	return answer;
 }
 
-console.log(parseInt('two hundred forty-six'));
+console.log(parseInt('one hundred twenty'));
