bbc0ab48c55e06d936fab5fe113e6b8eec65899d
Brendan Lentz <Brendan.Lentz@gmail.com>
2021-06-20 20:03:39 -0700

    About half the test cases are working, needs some more work

--- ExtractDomainName.js
+++ ExtractDomainName.js
@@ -1,7 +1,8 @@
 /* https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
  * Extract the Domain Name from the URL
  * 
- * Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+ * Write a function that when given a URL as a string, parses out just the
+ * domain name and returns it as a string. For example:
 
 domainName("http://github.com/carbonfive/raygun") == "github"
 domainName("http://www.zombie-bites.com") == "zombie-bites"
@@ -10,9 +11,47 @@ domainName("https://www.cnet.com") == "cnet"
 */
 
 function domainName(url){
-	console.log('Hi!!!');
+	console.log("original input is: " + url );	
+	// Strip out the http part
+	let tempAnswer = "";
+
+	if(url.match(/https:\/\/www.(.)*/)){
+		tempAnswer = url.replace("https://www.", "")
+		console.log("got inside first if");
+		console.log("tempAnswer is: " + tempAnswer);
+	}
+	else if (url.match(/https:\/\/(.)*/)){
+		tempAnswer = url.replace("https://", "" );
+		console.log("got inside second if");
+		console.log("tempAnswer is: " + tempAnswer);
+	}
+	else if (url.match(/http:\/\/www.(.)*/)){
+		tempAnswer = url.replace("http://www.", "")
+		console.log("got inside third if");
+		console.log("tempAnswer is: " + tempAnswer);
+	}
+	else if (url.match(/http:\/\/(.)*/)){
+		tempAnswer = url.replace("http://", "")
+		console.log("got inside fourth if");
+		console.log("tempAnswer is: " + tempAnswer);
+	}
+	
+	// If it has www, then strip that out too
+	if(url.match(/www.(.)*/)){
+		console.log("got inside the if statemenet")
+		tempAnswer = url.replace("www.", "");
+		}
+	let dotPosition = tempAnswer.indexOf('.');
+	console.log(dotPosition);
+	return tempAnswer.slice(0,dotPosition);
+
+	
 }
 
-domainName('hi');
+console.log(domainName("http://google.com") + "\n");
+console.log(domainName("www.xakep.ru") + "\n");
+console.log(domainName("https://youtube.com"));
+//console.log(domainName("http://www.zombie-bites.com"));
+//console.log(domainName("https://www.cnet.com"));
 
-Test.assertEquals(domainName("http://google.com"), "google");
+
