533c19ba78f795529c8897ec10482504eb60a4fe
Brendan Lentz <Brendan.Lentz@gmail.com>
2021-06-22 21:49:00 -0700

    adding this now, even though I'm going to revert back to 032955414, because the only way git is allowing me to revert, is to commit the current file first. I guess that is it's way of ensuring I don't lose changes. Once this gets committed I should be able to revert back to 032955414, using git revert 032955414

--- ExtractDomainName.js
+++ ExtractDomainName.js
@@ -11,49 +11,10 @@ domainName("https://www.cnet.com") == "cnet"
 */
 
 function domainName(url){
-	console.log("original input is: " + url );	
-	// Strip out the http part
-	let tempAnswer = "";
-
-	if(url.match(/https:\/\/www.(.)*/)){
-		tempAnswer = url.replace("https://www.", "")
-		console.log("got inside first if");
-		console.log("tempAnswer is: " + tempAnswer);
-	}
-	else if (url.match(/https:\/\/(.)*/)){
-		tempAnswer = url.replace("https://", "" );
-		console.log("got inside second if");
-		console.log("tempAnswer is: " + tempAnswer);
-	}
-	else if (url.match(/http:\/\/www.(.)*/)){
-		tempAnswer = url.replace("http://www.", "")
-		console.log("got inside third if");
-		console.log("tempAnswer is: " + tempAnswer);
-	}
-	else if (url.match(/http:\/\/(.)*/)){
-		tempAnswer = url.replace("http://", "")
-		console.log("got inside fourth if");
-		console.log("tempAnswer is: " + tempAnswer);
-	}
-	
-	// If it has www, then strip that out too
-	if(tempAnswer.match(/www.(.)*/)){
-		console.log("tempAnswer at the top of the if statement is: " + tempAnswer);
-		console.log("got inside the if statemenet")
-		tempAnswer = url.replace("www.", "");
-		console.log("tempAnswer at the bottom of the if statement is: " + tempAnswer);
-		}
-	let dotPosition = tempAnswer.indexOf('.');
-	console.log(dotPosition);
-	return tempAnswer.slice(0,dotPosition);
-
-	
+	console.log("got inside domainName function, and url is: " + url);
+	//console.log(url.toUpperCase());
+	return url.toUpperCase();
 }
 
-console.log(domainName("http://google.com") + "\n");
-console.log(domainName("www.xakep.ru") + "\n");
-console.log(domainName("https://youtube.com"));
-//console.log(domainName("http://www.zombie-bites.com"));
-//console.log(domainName("https://www.cnet.com"));
-
+console.log(domainName("www.google.com"));
 
