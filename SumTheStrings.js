b681140c3f60945ebd4701a945df631db66cefe9
Brendan Lentz <Brendan.Lentz@gmail.com>
2021-06-29 15:36:06 -0700

    code challenge during office hours

---
+++ SumTheStrings.js
@@ -0,0 +1,19 @@
+function sumStr(a,b) {
+  /*console.log("a is:" + a);
+  console.log("b is:" + b);
+  console.log("a is type: " + typeof a);
+  console.log("b is type:" + typeof b);
+  */
+  
+  let x = parseInt(a);
+  let y = parseInt(b);
+  
+  let sum = x +y;
+  //console.log("sum is: " + sum);
+  
+  let answer = sum.toString();
+  console.log('answer is: ' + answer)
+  console.log(answer);
+    
+  return answer;
+}
