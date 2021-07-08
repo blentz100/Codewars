59309d410e88f46d79c6a0c14af967e515de757e
Brendan Lentz <Brendan.Lentz@gmail.com>
2021-07-07 17:06:00 -0700

    added instructions

--- ConvertNumbertoReversedArray.js
+++ ConvertNumbertoReversedArray.js
@@ -1,4 +1,14 @@
 //https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/javascript
+/*
+ * Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number
+within an array in reverse order.
+
+Example:
+ */
+
+
+
 function digitize(n) {
   console.log("Inside digitize, n is: " + n);
   console.log("n is type: " + typeof n)
